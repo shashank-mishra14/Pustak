4a81625969841d8a7c266f2ccb971d4368e8ada5
Shashank Mishra <shashank.mishra.dev14@gmail.com>
2025-02-06 13:21:25 +0530

    [Added app constants]

---
+++ configs/constants.tsx
@@ -0,0 +1,11 @@
+import { Image } from "react-native";
+
+export const onboardingSlides: onBoardingSlidesTypes[] = [
+  {
+    color: "#40E0D0",
+    title: "Welcome to Pustak",
+    secondTitle: "Read your favorite books",
+    subTitle: "Read your favorite books on the go with Pustak",
+    image: <Image source={One} style={} />,
+  },
+];
